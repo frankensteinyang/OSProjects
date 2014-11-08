08ed45f2a7a8e68faaa83dd7f097580d2d5b6bc4
iHannibal <fredisstillalmighty@gmail.com>
2014-11-08 15:21:01 +0800

    Sat Nov 8 2014

---
+++ GroupPurchaseDemo/GroupPurchaseDemo/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  GroupPurchaseDemo
+//
+//  Created by Hannibal Yang on 11/8/14.
+//  Copyright (c) 2014 Hannibal Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 2;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    
+}
+
+@end

b6dc635458e550a23f3eed05aaeec51148658f02
iHannibal <fredisstillalmighty@gmail.com>
2014-11-14 23:55:09 +0800

    Fri Nov 14 23:55 2014

---
+++ StretchableAndResizable/StretchableAndResizable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StretchableAndResizable
+//
+//  Created by Hannibal Yang on 11/14/14.
+//  Copyright (c) 2014 Hannibal Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

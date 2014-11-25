6bdbb6311ff5e239ddc31c0766408f971ffaeaba
iHannibal <fredisstillalmighty@gmail.com>
2014-11-25 23:53:26 +0800

    Tue Nov 25 23:53 2014

---
+++ OnlineFoodOrdering/OnlineFoodOrdering/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnlineFoodOrdering
+//
+//  Created by Hannibal Yang on 11/25/14.
+//  Copyright (c) 2014 Hannibal Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OnlineFoodOrdering/OnlineFoodOrdering/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  OnlineFoodOrdering
+//
+//  Created by Hannibal Yang on 11/25/14.
+//  Copyright (c) 2014 Hannibal Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end

445af0ceb2be609f174a7ea1531f4410e16f0c9e
iHannibal <fredisstillalmighty@gmail.com>
2014-12-01 04:45:28 +0800

    Mon Dec 1 04:45 2014

--- Xcode61EmptyApplication/Xcode61EmptyApplication/AppDelegate.m
+++ Xcode61EmptyApplication/Xcode61EmptyApplication/AppDelegate.m
@@ -15,15 +15,39 @@
 
 @implementation AppDelegate
 
+/*
+UIViewController
+ 1.控制器内部的view是延迟加载
+ 1> 用到时再加载
+ 2> 加载完毕后会调用控制器的viewDidLoad方法
+ 
+ 2.创建控制器的方式
+ 1> 直接通过代码创建
+ MyFirstViewController *first = [[MyFirstViewController alloc] init];
+ 
+ 2> 通过storyboard创建控制器
+ * 加载storyboard文件
+ UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"MyFirstStoryboard" bundle:nil];
+ * 从storyboard中创建控制器
+ // 根据控制器在storyboard文件中storyboard id来创建
+MyFirstViewController *first = [storyboard instantiateViewControllerWithIdentifier:@"FrankensteinID"];
+ // 直接创建箭头所指的控制器（initial controller）
+  MyFirstViewController *first = [storyboard instantiateInitialViewController];
+ */
+
+
+
+
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.backgroundColor = [UIColor whiteColor];
+//    self.window.backgroundColor = [UIColor whiteColor];
     
 //    UIViewController *vc = [[UIViewController alloc] init];
-    MyFirstViewController *first = [[MyFirstViewController alloc] init];
+//    MyFirstViewController *first = [[MyFirstViewController alloc] init];
 //    vc.view.backgroundColor = [UIColor redColor];
 //    first.view.backgroundColor = [UIColor blueColor];
 //    
@@ -34,6 +58,13 @@
     
     // Xcode 3 之后苹果官方推荐用根控制器
 //    self.window.rootViewController = vc;
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"MyFirstStoryboard" bundle:nil];
+    
+//    MyFirstViewController *first = [storyboard instantiateViewControllerWithIdentifier:@"StoryboardID"];
+//    MyFirstViewController *first = [storyboard instantiateViewControllerWithIdentifier:@"FrankensteinID"];
+    // 直接创建箭头所指的控制器对象
+    MyFirstViewController *first = [storyboard instantiateInitialViewController];
+    
     self.window.rootViewController = first;
 //    [self.window addSubview:vc.view];
     

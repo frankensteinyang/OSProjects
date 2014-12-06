cd8cc4bb3451ae802193c80627120ed49aecc324
iHannibal <fredisstillalmighty@gmail.com>
2014-12-07 06:59:34 +0800

    Sun Dec 7 06:59 2014

---
+++ AspieContacts/AspieContacts/AspieContacts.h
@@ -0,0 +1,16 @@
+//
+//  AspieContacts.h
+//  AspieContacts
+//
+//  Created by Hannibal Yang on 12/7/14.
+//  Copyright (c) 2014 Hannibal Yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AspieContacts : NSObject <NSCoding>
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *phone;
+
+@end
--- AspieContacts/AspieContactsViewController.m
+++ AspieContacts/AspieContactsViewController.m
@@ -6,32 +6,68 @@
 //  Copyright (c) 2014 Hannibal Yang. All rights reserved.
 //
 
+#define kFilePath [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0] stringByAppendingPathComponent:@"contacts.data"]
+
 #import "AspieContactsViewController.h"
+#import "AspieContactsAddViewController.h"
+#import "AspieContacts.h"
 
-@interface AspieContactsViewController ()
-
+@interface AspieContactsViewController () <AspieContactsAddViewControllerDelegate, UITableViewDataSource> {
+    
+    NSMutableArray *_contacts;
+}
 @end
 
 @implementation AspieContactsViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    _contacts = [NSKeyedUnarchiver unarchiveObjectWithFile:kFilePath];
+    if (_contacts == nil) {
+        _contacts = [NSMutableArray array];
+    }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+
+    NSLog(@"Yah! %@ %@", segue.sourceViewController, segue.destinationViewController);
+    
+    UINavigationController *navigation = segue.destinationViewController;
+    // navigation.topViewController的属性为UIViewController，父类类型赋值给子类，要强制转换，segue.destinationViewController为id类型，故不警告
+    AspieContactsAddViewController *add = (AspieContactsAddViewController *)navigation.topViewController;
+    
+    // 让AspieContactsViewController成为AspieContactsAddViewController的代理
+    add.delegate = self;
+    
 }
-*/
+
+- (void)saveViewController:(AspieContactsAddViewController *)save didSaveContacts:(AspieContacts *)contacts {
+
+    [_contacts insertObject:contacts atIndex:0];
+    [self.tableView reloadData];
+    // 归档
+    NSLog(@"%@", kFilePath);
+    [NSKeyedArchiver archiveRootObject:_contacts toFile:kFilePath];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    static NSString *ID = @"AspieContactsID";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
+    }
+    AspieContacts *contacts = _contacts[indexPath.row];
+    cell.textLabel.text = contacts.name;
+    // detailTextLabel默认不显示，必须把Style设置为:UITableViewCellStyleSubtitle
+    cell.detailTextLabel.text = contacts.phone;
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return _contacts.count;
+}
 
 @end

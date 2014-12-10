9c6a52aab625348c059f07179da731bf233d48e0
iHannibal <fredisstillalmighty@gmail.com>
2014-12-11 06:30:32 +0800

    Thu Dec 11 06:30 2014

---
+++ HardestGameFrankenstein/HardestGameFrankenstein/Pause/HGFPauseViewController.h
@@ -0,0 +1,17 @@
+//
+//  HGFPauseViewController.h
+//  HardestGameFrankenstein
+//
+//  Created by Hannibal Yang on 12/11/14.
+//  Copyright (c) 2014 Hannibal Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HGFPauseViewController : UIViewController
+
+- (IBAction)resume;
+
+- (IBAction)home;
+
+@end
---
+++ HardestGameFrankenstein/HardestGameFrankenstein/Play/HGFPlayViewController.h
@@ -0,0 +1,15 @@
+//
+//  HGFPlayViewController.h
+//  HardestGameFrankenstein
+//
+//  Created by Hannibal Yang on 12/11/14.
+//  Copyright (c) 2014 Hannibal Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HGFPlayViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIView *footView;
+
+@end

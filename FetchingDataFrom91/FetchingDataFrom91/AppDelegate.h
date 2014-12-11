5709d879bfbbce11284084df30559bbe92913f02
iHannibal <fredisstillalmighty@gmail.com>
2014-12-12 05:38:47 +0800

    Fri Dec 12 05:38 2014

---
+++ FetchingDataFrom91/FetchingDataFrom91/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FetchingDataFrom91
+//
+//  Created by Hannibal Yang on 12/12/14.
+//  Copyright (c) 2014 Hannibal Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FetchingDataFrom91/FetchingDataFrom91/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FetchingDataFrom91
+//
+//  Created by Hannibal Yang on 12/12/14.
+//  Copyright (c) 2014 Hannibal Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

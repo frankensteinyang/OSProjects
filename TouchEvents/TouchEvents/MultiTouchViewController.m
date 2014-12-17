fd5a3c728990d288601dd2b65db6690ce4552592
iHannibal <fredisstillalmighty@gmail.com>
2014-12-18 01:31:18 +0800

    Thu Dec 18 01:31 2014

--- TouchEvents/TouchEvents/MultiTouchViewController.m
+++ TouchEvents/TouchEvents/MultiTouchViewController.m
@@ -51,16 +51,17 @@
         CGPoint location = [touch locationInView:self.view];
         UIImageView *imageView = [[UIImageView alloc] initWithImage:_images[i]];
         imageView.center = location;
+        [self.view addSubview:imageView];
         
         // 动画，让图片的透明度降低，逐渐消失
         [UIView animateWithDuration:1.0f animations:^{
             imageView.alpha = 0.5f;
             
         } completion:^(BOOL finished) {
+            // block中的程序，是一组预先准备好的代码，不会马上执行
             [imageView removeFromSuperview];
         }];
         
-        [self.view addSubview:imageView];
         i++;
     }
 }
---
+++ TouchEvents/TouchEvents/ResponderChainViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResponderChainViewController.h
+//  TouchEvents
+//
+//  Created by Hannibal Yang on 12/18/14.
+//  Copyright (c) 2014 Hannibal Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResponderChainViewController : UIViewController
+
+@end

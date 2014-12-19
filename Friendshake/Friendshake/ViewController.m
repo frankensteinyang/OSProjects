06b4b585c34623a490a19635cdd18850f6593dde
iHannibal <fredisstillalmighty@gmail.com>
2014-12-19 10:22:42 +0800

    Fri Dec 19 10:22 2014

--- Friendshake/Friendshake/ViewController.m
+++ Friendshake/Friendshake/ViewController.m
@@ -14,6 +14,16 @@
 
 @implementation ViewController
 
+/*
+ WeChat摇一摇的实现思路：
+ 1. 进入界面，利用CoreLocation（MapKit）判断用户当前的位置
+ 2. 用户摇动，将用户的经纬度，用户帐号，摇晃时间，发送到WeChat的服务器
+ 3. 1）后台处理，计算当前时间的一定范围内摇晃手机的用户
+    2）产生列表，发送到用户手机
+ 4. 用户接收到数据后，使用UITableView展现数据
+ */
+
+// 成为第一响应者是监听摇晃事件的关键
 // 成为第一响应者
 - (BOOL)canBecomeFirstResponder {
 
@@ -37,7 +47,7 @@
 - (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event {
 
     if (motion == UIEventSubtypeMotionShake) {
-        NSLog(@"摇晃了！");
+        NSLog(@"已摇晃！");
     }
 }
 

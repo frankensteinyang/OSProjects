1d47608547b28b618ed6156a58c44f63a0f5e9fd
iHannibal <fredisstillalmighty@gmail.com>
2014-12-22 14:18:40 +0800

    Mon Dec 22 14:18 2014

---
+++ EPainter/EPainter/EPColorView.h
@@ -0,0 +1,13 @@
+//
+//  EPColorView.h
+//  EPainter
+//
+//  Created by Hannibal Yang on 12/22/14.
+//  Copyright (c) 2014 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EPColorView : UIView
+
+@end
--- EPainter/EPainter/EPToolsView.m
+++ EPainter/EPainter/EPToolsView.m
@@ -8,13 +8,27 @@
 
 #import "EPToolsView.h"
 #import "EPLinesView.h"
+#import "EPColorView.h"
 
-#define kButtonHeight 44
+#define kCommonHeight 44
+
+typedef enum {
+
+    kToolsViewColor = 0,
+    kToolsViewLineWidth,
+    kToolsViewEraser,
+    kToolsViewUndo,
+    kToolsViewClear,
+    kToolsViewPhoto,
+    kToolsViewSave
+    
+} kToolsViewFunction;
 
 @interface EPToolsView()
 
 @property (weak, nonatomic) UIButton *preSelectedBtn;
 @property (weak, nonatomic) EPLinesView *selectedLine;
+@property (weak, nonatomic) EPColorView *colorView;
 
 @end
 
@@ -24,10 +38,11 @@
 
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = [UIColor brownColor];
+        self.backgroundColor = [UIColor lightGrayColor];
         [self createMenuBar];
     }
     return self;
+    
 }
 
 #pragma mark - 创建界面工具栏
@@ -38,17 +53,19 @@
     CGFloat btnWidth = self.bounds.size.width / array.count;
     CGFloat y = IS_IOS7 ? 20 : 0;
     
-    // Block遍历
+    // Block循环遍历
     [array enumerateObjectsUsingBlock:^(NSString *text, NSUInteger idx, BOOL *stop) {
         UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
         [btn setTitle:text forState:UIControlStateNormal];
         CGFloat x = idx * btnWidth;
-        [btn setFrame:CGRectMake(x, y, btnWidth, kButtonHeight)];
+        [btn setFrame:CGRectMake(x, y, btnWidth, kCommonHeight)];
         
         // 设置按钮属性
         [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         [btn.titleLabel setFont:[UIFont systemFontOfSize:14]];
         [btn setTitleColor:[UIColor blueColor] forState:UIControlStateSelected];
+        
+        btn.tag = idx;
         [self addSubview:btn];
         
         [btn addTarget:self action:@selector(tapButton:) forControlEvents:UIControlEventTouchUpInside];
@@ -67,6 +84,23 @@
     [button setSelected:YES];
     self.preSelectedBtn = button;
     
+    // 绘制下划线
+    [self drawUnderlineWithBtn:button];
+    
+    switch (button.tag) {
+        case kToolsViewColor:
+            [self showOrHideColorView];
+            break;
+            
+        default:
+            break;
+    }
+    
+}
+
+#pragma mark - 绘制按钮底部下划线
+- (void)drawUnderlineWithBtn:(UIButton *)button {
+
     // 计算下划线的位置
     CGFloat x = button.frame.origin.x + button.titleLabel.frame.origin.x;
     CGFloat y = button.frame.origin.y + button.titleLabel.frame.origin.y + button.titleLabel.frame.size.height + 2.0f;
@@ -81,6 +115,38 @@
     
     [UIView animateWithDuration:0.4f animations:^{
         [self.selectedLine setFrame:CGRectMake(x, y, w, 2.0)];
+    }];
+    
+}
+
+#pragma mark - 子视图操作
+// 显示/隐藏颜色视图
+- (void)showOrHideColorView {
+
+    if (self.colorView == nil) {
+        // 初始在屏幕外面-self.bounds.size.height
+        EPColorView *view = [[EPColorView alloc] initWithFrame:CGRectMake(0, -self.bounds.size.height, self.bounds.size.width, kCommonHeight)];
+        
+        // 在视图的最底层插入视图
+        [self insertSubview:view atIndex:0];
+        
+        self.colorView = view;
+    }
+    
+    // 目标位置
+    CGRect frame = self.frame;
+    CGRect colorFrame = self.colorView.frame;
+    if (colorFrame.origin.y < 0) {
+        colorFrame.origin.y = self.bounds.size.height;
+        frame.size.height = kToolsViewHeight + colorFrame.size.height;
+    } else {
+        colorFrame.origin.y = -self.bounds.size.height;
+        frame.size.height = kToolsViewHeight;
+    }
+    
+    [UIView animateWithDuration:0.4f animations:^{
+        [self.colorView setFrame:colorFrame];
+        self.frame = frame;
     }];
     
 }

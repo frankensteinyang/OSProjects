4ff316a50ad2c2034d0cd308d10312e642a05e91
iHannibal <fredisstillalmighty@gmail.com>
2014-12-24 22:40:41 +0800

    Wed Dec 24 22:40 2014

--- UploadAttachment/UploadAttachment/ViewController.m
+++ UploadAttachment/UploadAttachment/ViewController.m
@@ -7,23 +7,41 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 
 @interface ViewController ()
 
 @end
-
-@implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
 
 /*
  接口名：http://panfile.xuele.net/mupload.ashx?U=教师ID&Type=文件扩展名&Size=文件大小&Name=文件名&Aid=课程ID
  接口方式：Post
  返回值：文件ID
  */
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    NSURL *URL = [NSURL URLWithString:@"http://panfile.xuele.net/mupload.ashx"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    
+    NSURL *filePath = [NSURL fileURLWithPath:@"file://path/to/image.png"];
+    NSURLSessionUploadTask *uploadTask = [manager uploadTaskWithRequest:request fromFile:filePath progress:nil completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
+        if (error) {
+            NSLog(@"Error: %@", error);
+        } else {
+            NSLog(@"Success: %@ %@", response, responseObject);
+        }
+    }];
+    [uploadTask resume];
+    
+}
+
 #pragma mark - 文件上传
 - (IBAction)uploadAttachment {
     

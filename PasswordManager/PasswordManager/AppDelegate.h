9304ea460818c0d8da1d03edccc8678de28f1bf7
iHannibal <fredisstillalmighty@gmail.com>
2014-12-31 23:17:42 +0800

    Wed Dec 31 23:17 2014

---
+++ PasswordManager/PasswordManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PasswordManager
+//
+//  Created by Hannibal Yang on 12/31/14.
+//  Copyright (c) 2014 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PasswordManager/PasswordManager/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  PasswordManager
+//
+//  Created by Hannibal Yang on 12/31/14.
+//  Copyright (c) 2014 Frankenstein Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end

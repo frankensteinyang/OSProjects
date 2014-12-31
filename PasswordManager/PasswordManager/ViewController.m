6a632d5d7b5af3fc5bbc97018ed9073e9147c03b
iHannibal <fredisstillalmighty@gmail.com>
2015-01-01 03:13:09 +0800

    Thu Jan 1 03:13 2014

--- PasswordManager/PasswordManager/ViewController.m
+++ PasswordManager/PasswordManager/ViewController.m
@@ -10,7 +10,11 @@
 #import <sqlite3.h>
 #import "NSString+DocDir.h"
 
-@interface ViewController ()
+@interface ViewController () {
+
+    // 数据库句柄
+    sqlite3 *_db;
+}
 
 @end
 
@@ -19,23 +23,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self openDB];
+    [self createTable];
     
 }
 
-
+#pragma mark 打开数据库
 - (void)openDB {
 
     // 设置沙盒中的文件路径
     NSString *dbPath = [@"Frankenstein" appendDocumentDir];
     NSLog(@"#%@", dbPath);
     
-    sqlite3 *db;
-    
-    if (SQLITE_OK == sqlite3_open([dbPath UTF8String], &db)) {
+    _db = NULL;
+    if (SQLITE_OK == sqlite3_open([dbPath UTF8String], &_db)) {
         NSLog(@"数据库打开成功！");
     } else {
         NSLog(@"数据库打开失败！");
     }
     
 }
+
+#pragma mark 创建数据表
+- (void)createTable {
+
+    // 定义SQL语句
+    NSString *sql = @"CREATE TABLE IF NOT EXISTS PasswordManager (ID integer NOT NULL, Account text, Password text, Info blob, PRIMARY KEY(ID))";
+    // 执行SQL
+    char *errmsg = NULL;
+    if (SQLITE_OK == sqlite3_exec(_db, [sql UTF8String], NULL, NULL, &errmsg)) {
+        NSLog(@"建表成功！");
+    } else {
+        NSLog(@"建表失败！ - %s", errmsg);
+    }
+    
+}
+
+#pragma mark 插入数据
+- (void)insertData {
+
+    
+}
+
 @end

696e6bb2c938e8df912215d403e80a5cc3335749
iHannibal <fredisstillalmighty@gmail.com>
2015-01-01 23:34:55 +0800

    23:34

--- PasswordManager/PasswordManager/ViewController.m
+++ PasswordManager/PasswordManager/ViewController.m
@@ -27,16 +27,17 @@
     [self createTable];
     Password *p = [Password passwordWithID:0 account:@"fredisalmighty@hotmail.com" password:@"jinzhong" info:@"密保问题不是这个。"];
     [self insertData:p];
+    [self allPasswords];
 }
 
 #pragma mark 单步执行SQL
-- (void)executeSQL:(NSString *)sql {
+- (void)executeSQL:(NSString *)sql message:(NSString *)msg {
 
     char *errmsg = NULL;
     if (SQLITE_OK == sqlite3_exec(_db, [sql UTF8String], NULL, NULL, &errmsg)) {
-        NSLog(@"操作成功！");
+        NSLog(@"%@成功！", msg);
     } else {
-        NSLog(@"操作失败！");
+        NSLog(@"%@失败！ - %s", msg, errmsg);
     }
     
 }
@@ -62,7 +63,7 @@
 
     // 定义SQL语句
     NSString *sql = @"CREATE TABLE IF NOT EXISTS PasswordManager (ID integer NOT NULL, account text, password text, info text, PRIMARY KEY(ID))";
-    [self executeSQL:sql];
+    [self executeSQL:sql message:@"创建数据表"];
     
 }
 
@@ -70,7 +71,20 @@
 - (void)insertData:(Password *)password {
 
     NSString *sql = [NSString stringWithFormat:@"INSERT INTO PasswordManager (account, password, info) VALUES ('%@', '%@', '%@')", password.account, password.password, password.info];
-    [self executeSQL:sql];
+    [self executeSQL:sql message:@"添加密码"];
+    
+}
+
+#pragma mark 查询所有密码
+- (void)allPasswords {
+
+    NSString *sql = @"SELECT * FROM PasswordManager";
+    sqlite3_stmt *stmt = NULL;
+    if (SQLITE_OK == sqlite3_prepare_v2(_db, [sql UTF8String], -1, &stmt, NULL)) {
+        NSLog(@"查询语句正确！");
+    } else {
+        NSLog(@"查询语句错误！");
+    }
     
 }
 

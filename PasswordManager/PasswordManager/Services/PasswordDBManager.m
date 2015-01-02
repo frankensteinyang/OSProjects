027b934196518de043a7cf757c00581357373bff
iHannibal <fredisstillalmighty@gmail.com>
2015-01-03 00:54:22 +0800

    Sat Jan 3 00:54 2015

--- PasswordManager/PasswordManager/Services/PasswordDBManager.m
+++ PasswordManager/PasswordManager/Services/PasswordDBManager.m
@@ -7,12 +7,23 @@
 //
 
 #import "PasswordDBManager.h"
+#import <sqlite3.h>
+#import "NSString+DocDir.h"
 
 // 全局变量
 static PasswordDBManager *_instance;
 
+@interface PasswordDBManager() {
+    
+    // 全局数据库句柄
+    sqlite3 *_db;
+}
+
+@end
+
 @implementation PasswordDBManager
 
+#pragma mark - 单例
 /**
  单例:
  1. 全局变量
@@ -37,8 +48,61 @@ static PasswordDBManager *_instance;
     return _instance;
 }
 
+#pragma mark - 数据库操作
+- (id)init {
+
+    self = [super init];
+    
+    // 懒加载数据库连接及创建数据表
+    if (self) {
+        [self openDB];
+        [self createTable];
+    }
+    return self;
+}
+
+#pragma mark 打开数据库
+- (void)openDB {
+    
+    // 设置沙盒中的文件路径
+    NSString *dbPath = [@"Frankenstein.db" appendDocumentDir];
+    NSLog(@"#%@", dbPath);
+    
+    _db = NULL;
+    if (SQLITE_OK == sqlite3_open([dbPath UTF8String], &_db)) {
+        NSLog(@"数据库打开成功！");
+    } else {
+        NSLog(@"数据库打开失败！");
+    }
+    
+}
+
+#pragma mark 创建数据表
+- (void)createTable {
+    
+    // 定义SQL语句
+    NSString *sql = @"CREATE TABLE IF NOT EXISTS PasswordManager (ID integer NOT NULL, account text, password text, info text, PRIMARY KEY(ID))";
+    [self executeSQL:sql message:@"创建数据表"];
+    
+}
+
+#pragma mark 单步执行SQL
+- (void)executeSQL:(NSString *)sql message:(NSString *)msg {
+    
+    char *errmsg = NULL;
+    if (SQLITE_OK == sqlite3_exec(_db, [sql UTF8String], NULL, NULL, &errmsg)) {
+        NSLog(@"%@成功！", msg);
+    } else {
+        NSLog(@"%@失败！ - %s", msg, errmsg);
+    }
+    
+}
+
+#pragma mark - 成员方法
 - (void)addPassword:(Password *)password {
 
+    NSString *sql = [NSString stringWithFormat:@"INSERT INTO PasswordManager (account, password, info) VALUES ('%@', '%@', '%@')", password.account, password.password, password.info];
+    [self executeSQL:sql message:@"添加密码"];
 }
 
 - (void)removePassword:(NSInteger)passwordID {
@@ -51,7 +115,33 @@ static PasswordDBManager *_instance;
 
 - (NSArray *)allPasswords {
 
-    return nil;
+    NSString *sql = @"SELECT ID, account, password, info FROM PasswordManager ORDER BY account";
+    sqlite3_stmt *stmt = NULL;
+    
+    NSMutableArray *passwords = nil;
+    
+    if (SQLITE_OK == sqlite3_prepare_v2(_db, [sql UTF8String], -1, &stmt, NULL)) {
+        NSLog(@"查询语句正确！");
+        
+        passwords = [NSMutableArray array];
+        
+        while (SQLITE_ROW == sqlite3_step(stmt)) {
+            int ID = sqlite3_column_int(stmt, 0);
+            const unsigned char *account = sqlite3_column_text(stmt, 1);
+            NSString *accountUTF8 = [NSString stringWithUTF8String:(const char *)account];
+            const unsigned char *password = sqlite3_column_text(stmt, 2);
+            NSString *passwordUTF8 = [NSString stringWithUTF8String:(const char *)password];
+            const unsigned char *info = sqlite3_column_text(stmt, 3);
+            NSString *infoUTF8 = [NSString stringWithUTF8String:(const char *)info];
+            
+            Password *p = [Password passwordWithID:ID account:accountUTF8 password:passwordUTF8 info:infoUTF8];
+            [passwords addObject:p];
+            NSLog(@"%@", p);
+        }
+    } else {
+        NSLog(@"查询语句错误！");
+    }
+    return passwords;
 }
 
 @end
--- PasswordManager/PasswordManager/ViewController.m
+++ PasswordManager/PasswordManager/ViewController.m
@@ -7,14 +7,12 @@
 //
 
 #import "ViewController.h"
-#import <sqlite3.h>
-#import "NSString+DocDir.h"
+#import "PasswordDBManager.h"
 #import "Password.h"
 
-@interface ViewController () {
+@interface ViewController () <UITableViewDataSource> {
 
-    // 数据库句柄
-    sqlite3 *_db;
+    NSArray *_passwordList;
 }
 
 @end
@@ -23,8 +21,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self openDB];
-    [self createTable];
+    _passwordList = [[PasswordDBManager sharedPasswordDBManager] allPasswords];
     
 //    NSArray *array = @[@"frankenstein@gmail.com", @"56464", @"wenti"];
 //    for (NSInteger i = 0; i < 10; i++) {
@@ -34,77 +31,93 @@
 //        Password *p = [Password passwordWithID:0 account:account password:password info:info];
 //        [self insertData:p];
 //    }
-    [self allPasswords];
 }
 
-#pragma mark 单步执行SQL
-- (void)executeSQL:(NSString *)sql message:(NSString *)msg {
+#pragma mark - 数据源方法
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
 
-    char *errmsg = NULL;
-    if (SQLITE_OK == sqlite3_exec(_db, [sql UTF8String], NULL, NULL, &errmsg)) {
-        NSLog(@"%@成功！", msg);
-    } else {
-        NSLog(@"%@失败！ - %s", msg, errmsg);
-    }
-    
+    static NSString *ID = @"passwordCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    Password *p = _passwordList[indexPath.row];
+    cell.textLabel.text = p.account;
+    NSString *str = [NSString stringWithFormat:@"密码：%@ 密保信息：%@", p.password, p.info];
+    cell.detailTextLabel.text = str;
+    return cell;
 }
 
-#pragma mark 打开数据库
-- (void)openDB {
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 
-    // 设置沙盒中的文件路径
-    NSString *dbPath = [@"Frankenstein.db" appendDocumentDir];
-    NSLog(@"#%@", dbPath);
-    
-    _db = NULL;
-    if (SQLITE_OK == sqlite3_open([dbPath UTF8String], &_db)) {
-        NSLog(@"数据库打开成功！");
-    } else {
-        NSLog(@"数据库打开失败！");
-    }
-    
+    return _passwordList.count;
 }
 
-#pragma mark 创建数据表
-- (void)createTable {
+//#pragma mark 单步执行SQL
+//- (void)executeSQL:(NSString *)sql message:(NSString *)msg {
+//
+//    char *errmsg = NULL;
+//    if (SQLITE_OK == sqlite3_exec(_db, [sql UTF8String], NULL, NULL, &errmsg)) {
+//        NSLog(@"%@成功！", msg);
+//    } else {
+//        NSLog(@"%@失败！ - %s", msg, errmsg);
+//    }
+//    
+//}
 
-    // 定义SQL语句
-    NSString *sql = @"CREATE TABLE IF NOT EXISTS PasswordManager (ID integer NOT NULL, account text, password text, info text, PRIMARY KEY(ID))";
-    [self executeSQL:sql message:@"创建数据表"];
-    
-}
+//#pragma mark 打开数据库
+//- (void)openDB {
+//
+//    // 设置沙盒中的文件路径
+//    NSString *dbPath = [@"Frankenstein.db" appendDocumentDir];
+//    NSLog(@"#%@", dbPath);
+//    
+//    _db = NULL;
+//    if (SQLITE_OK == sqlite3_open([dbPath UTF8String], &_db)) {
+//        NSLog(@"数据库打开成功！");
+//    } else {
+//        NSLog(@"数据库打开失败！");
+//    }
+//    
+//}
+//
+//#pragma mark 创建数据表
+//- (void)createTable {
+//
+//    // 定义SQL语句
+//    NSString *sql = @"CREATE TABLE IF NOT EXISTS PasswordManager (ID integer NOT NULL, account text, password text, info text, PRIMARY KEY(ID))";
+//    [self executeSQL:sql message:@"创建数据表"];
+//    
+//}
+//
+//#pragma mark 插入数据
+//- (void)insertData:(Password *)password {
+//
+//    NSString *sql = [NSString stringWithFormat:@"INSERT INTO PasswordManager (account, password, info) VALUES ('%@', '%@', '%@')", password.account, password.password, password.info];
+//    [self executeSQL:sql message:@"添加密码"];
+//    
+//}
 
-#pragma mark 插入数据
-- (void)insertData:(Password *)password {
-
-    NSString *sql = [NSString stringWithFormat:@"INSERT INTO PasswordManager (account, password, info) VALUES ('%@', '%@', '%@')", password.account, password.password, password.info];
-    [self executeSQL:sql message:@"添加密码"];
-    
-}
-
-#pragma mark 查询所有密码
-- (void)allPasswords {
-
-    NSString *sql = @"SELECT ID, account, password, info FROM PasswordManager";
-    sqlite3_stmt *stmt = NULL;
-    if (SQLITE_OK == sqlite3_prepare_v2(_db, [sql UTF8String], -1, &stmt, NULL)) {
-        NSLog(@"查询语句正确！");
-        while (SQLITE_ROW == sqlite3_step(stmt)) {
-            int ID = sqlite3_column_int(stmt, 0);
-            const unsigned char *account = sqlite3_column_text(stmt, 1);
-            NSString *accountUTF8 = [NSString stringWithUTF8String:(const char *)account];
-            const unsigned char *password = sqlite3_column_text(stmt, 2);
-            NSString *passwordUTF8 = [NSString stringWithUTF8String:(const char *)password];
-            const unsigned char *info = sqlite3_column_text(stmt, 3);
-            NSString *infoUTF8 = [NSString stringWithUTF8String:(const char *)info];
-            
-            Password *p = [Password passwordWithID:ID account:accountUTF8 password:passwordUTF8 info:infoUTF8];
-            NSLog(@"%@", p);
-        }
-    } else {
-        NSLog(@"查询语句错误！");
-    }
-    
-}
+//#pragma mark 查询所有密码
+//- (void)allPasswords {
+//
+//    NSString *sql = @"SELECT ID, account, password, info FROM PasswordManager";
+//    sqlite3_stmt *stmt = NULL;
+//    if (SQLITE_OK == sqlite3_prepare_v2(_db, [sql UTF8String], -1, &stmt, NULL)) {
+//        NSLog(@"查询语句正确！");
+//        while (SQLITE_ROW == sqlite3_step(stmt)) {
+//            int ID = sqlite3_column_int(stmt, 0);
+//            const unsigned char *account = sqlite3_column_text(stmt, 1);
+//            NSString *accountUTF8 = [NSString stringWithUTF8String:(const char *)account];
+//            const unsigned char *password = sqlite3_column_text(stmt, 2);
+//            NSString *passwordUTF8 = [NSString stringWithUTF8String:(const char *)password];
+//            const unsigned char *info = sqlite3_column_text(stmt, 3);
+//            NSString *infoUTF8 = [NSString stringWithUTF8String:(const char *)info];
+//            
+//            Password *p = [Password passwordWithID:ID account:accountUTF8 password:passwordUTF8 info:infoUTF8];
+//            NSLog(@"%@", p);
+//        }
+//    } else {
+//        NSLog(@"查询语句错误！");
+//    }
+//    
+//}
 
 @end

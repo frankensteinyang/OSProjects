d68c0aff643ee7fa182d7ddbcdd53e78efc2732b
iHannibal <fredisstillalmighty@gmail.com>
2015-01-03 23:35:56 +0800

    Sat Jan 3 23:35 2015

---
+++ TicketingSystem/TicketingSystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicketingSystem
+//
+//  Created by Frankenstein Yang on 1/3/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TicketingSystem/TicketingSystem/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TicketingSystem
+//
+//  Created by Frankenstein Yang on 1/3/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

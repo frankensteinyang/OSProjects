ba78e9b0493aeb243b5a50b4e833c7e2b00281fd
iHannibal <fredisstillalmighty@gmail.com>
2015-01-04 23:22:21 +0800

    Sun Jan 4 23:22 2015

---
+++ TicketingSystem/TicketingSystem/Ticket.h
@@ -0,0 +1,15 @@
+//
+//  Ticket.h
+//  TicketingSystem
+//
+//  Created by Frankenstein Yang on 1/4/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Ticket : NSObject
+
+@property (nonatomic, assign) NSInteger tickets;
+
+@end
---
+++ TicketingSystem/TicketingSystem/Ticket.m
@@ -0,0 +1,13 @@
+//
+//  Ticket.m
+//  TicketingSystem
+//
+//  Created by Frankenstein Yang on 1/4/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "Ticket.h"
+
+@implementation Ticket
+
+@end

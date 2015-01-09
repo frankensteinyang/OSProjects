7ed790f4eb5bd1b628029916d232bfe35b2d9f92
iHannibal <fredisstillalmighty@gmail.com>
2015-01-09 21:53:31 +0800

    ASI的同步和异步请求

--- ASI/ASI/ViewController.m
+++ ASI/ASI/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "ASIHTTPRequest.h"
 
-@interface ViewController ()
+@interface ViewController () <ASIHTTPRequestDelegate>
+
+@property (nonatomic, strong) ASIHTTPRequest *request;
 
 @end
 
@@ -19,9 +22,79 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    [self sync];
+    [self async];
+}
+
+#pragma mark - 同步请求
+- (void)sync {
+
+    // 创建请求对象
+    NSURL *url = [NSURL URLWithString:@"http://localhost:8080/FrankensteinServer/login?username=123&pwd=123"];
+    ASIHTTPRequest *request = [ASIHTTPRequest requestWithURL:url];
+    
+    request.timeOutSeconds = 10;
+    
+    // 发送请求
+    [request startSynchronous];
+    
+    // 检测服务器返回的结果
+    if (request.error) {
+        NSLog(@"请求出错 %@", request.error);
+    } else {
+        NSLog(@"%d %@", request.responseStatusCode, request.responseStatusMessage);
+        NSLog(@"%d", request.responseData.length);
+        NSLog(@"%@", request.responseString);
+        NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:request.responseData options:NSJSONReadingMutableLeaves error:nil];
+        NSLog(@"%@", dict);
+        
+    }
+    
+}
+
+- (void)dealloc {
+    // 重点（必须在控制器销毁的时候，取消请求/清除代理）
+    [self.request clearDelegatesAndCancel];
+}
+
+#pragma mark - 异步请求
+- (void)async {
+
+    // 创建请求对象
+    NSURL *url = [NSURL URLWithString:@"http://localhost:8080/FrankensteinServer/login?username=123&pwd=123"];
+    self.request = [ASIHTTPRequest requestWithURL:url];
+    
+    self.request.timeOutSeconds = 10;
+    
+    // 设置代理
+    self.request.delegate = self;
+    
+    // 发送请求
+    [self.request startAsynchronous];
+    
+}
+
+#pragma mark - 代理方法
+- (void)requestStarted:(ASIHTTPRequest *)request {
+
+    NSLog(@"requestStarted");
+}
+
+- (void)request:(ASIHTTPRequest *)request didReceiveData:(NSData *)data {
+
+    NSLog(@"didReceiveData");
+}
+
+- (void)requestFinished:(ASIHTTPRequest *)request {
+
+    NSLog(@"requestFinished");
+}
+
+- (void)requestFailed:(ASIHTTPRequest *)request {
+
+    NSLog(@"requestFailed");
 }
 
 @end

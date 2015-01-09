6fa22637571835294ab08c9dd763e3dafb0c5927
iHannibal <fredisstillalmighty@gmail.com>
2015-01-09 20:42:15 +0800

    Fri Jan 9 20:42 2015

---
+++ ASI/ASI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASI
+//
+//  Created by Frankenstein Yang on 1/9/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ASI/ASI/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ASI
+//
+//  Created by Frankenstein Yang on 1/9/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

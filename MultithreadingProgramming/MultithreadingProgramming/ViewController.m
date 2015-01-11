e4d2b9ab201a34f3ce6f0c960a2a904bc7862c7c
iHannibal <fredisstillalmighty@gmail.com>
2015-01-12 02:50:18 +0800

    线程的生命周期

--- MultithreadingProgramming/MultithreadingProgramming/ViewController.m
+++ MultithreadingProgramming/MultithreadingProgramming/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <pthread.h>
 
 @interface ViewController ()
+
+@property (nonatomic, strong) NSThread *thread;
 
 - (IBAction)btnClick;
 
@@ -18,19 +21,103 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    /**
+     'NSInvalidArgumentException', reason: '*** -[NSThread start]: attempt to start the thread again'
+     :returns: 生命周期死亡
+     */
+    self.thread = [[NSThread alloc] initWithTarget:self selector:@selector(lifeCycle) object:nil];
+    self.thread.name = @"线程的生命周期探究";
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+/**
+ *  生命周期
+ *
+ *  @param IBAction LifeCycle
+ *
+ *  @return nil
+ */
+- (IBAction)btnLifeCycle {
+    
+    [self.thread start];
+    
 }
 
+- (void)lifeCycle {
+
+    NSLog(@"开始 %@", [NSThread currentThread].name);
+    // 阻塞状态
+//    [NSThread sleepForTimeInterval:5];
+    
+    // 睡眠
+//    NSDate *date = [NSDate dateWithTimeIntervalSinceNow:5.0];
+//    [NSThread sleepUntilDate:date];
+    
+    for (int i = 0; i < 1000; i++) {
+        NSLog(@"%d %@", i, [NSThread currentThread].name);
+        if (i == 50) {
+            // [NSThread exit] = return
+            [NSThread exit];
+        }
+    }
+    NSLog(@"结束 %@", [NSThread currentThread].name);
+    
+}
+
+/**
+ *  NSThread
+ */
+- (IBAction)btnNSThread {
+    
+    NSThread *current = [NSThread currentThread];
+    NSLog(@"%@", current);
+    NSThread *threadA = [[NSThread alloc] initWithTarget:self selector:@selector(run:) object:@"Frankenstein"];
+    threadA.name = @"线程A";
+    [threadA start];
+    
+    NSThread *threadB = [[NSThread alloc] initWithTarget:self selector:@selector(run:) object:@"Frankie"];
+    threadB.name = @"线程B";
+    [threadB start];
+    
+    // 创建完线程自动启动
+    [NSThread detachNewThreadSelector:@selector(run:) toTarget:self withObject:@"I am a param"];
+    
+}
+
+- (void)run:(NSString *)param {
+
+    for (int i = 0; i < 20000; i++) {
+        NSLog(@"%@ %@", [NSThread currentThread], param);
+    }
+    
+}
+
+/**
+ *  pthread研究
+ */
 - (IBAction)btnClick {
     
     // 获得当前的线程
     NSThread *current = [NSThread currentThread];
+    // 执行耗时操作，创建一条子线程
+    pthread_t threadID;
+    pthread_create(&threadID, NULL, run, NULL);
     NSLog(@"%@", current);
+//    for (int i = 0; i < 20000; i++) {
+//        NSLog(@"%@", current);
+//    }
     
 }
+
+// C语言函数
+void *run(void *data) {
+
+    NSThread *current = [NSThread currentThread];
+    for (int i = 0; i < 20000; i++) {
+        NSLog(@"%@", current);
+    }
+    return NULL;
+    
+}
+
 @end
--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # MultithreadingProgramming
 iOS MultithreadingProgramming.
 <NSThread: 0x7a71f9a0>{number = 1, name = main}
+
+pthread.h : 
+
+NSThread :
+
+线程的状态（生命周期）:
+创建 内存 启动 
+新建状态的线程不参与调度
+可调度线程池在内存
+start方法把线程放进线程池
+新建>start 就绪>CPU调度>运行
+线程阻塞，会被清除出调度池
+死亡状态不代表被清除出内存，只是不在调度池
+新建 就绪 运行 阻塞 死亡

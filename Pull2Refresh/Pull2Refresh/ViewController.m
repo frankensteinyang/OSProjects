a5aa5bf099fc099174bb222266f691eed1240b81
iHannibal <fredisstillalmighty@gmail.com>
2015-01-14 00:53:17 +0800

    下拉刷新和上拉加载更多

--- Pull2Refresh/Pull2Refresh/ViewController.m
+++ Pull2Refresh/Pull2Refresh/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "MJRefresh.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -16,7 +19,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 集成刷新控件（下拉上拉）
+    [self setupRefresh];
+    
+}
+
+- (void)setupRefresh {
+    
+    [self.tableView addHeaderWithTarget:self action:@selector(headerRefreshing) dateKey:@"table"];
+    [self.tableView headerBeginRefreshing];
+    [self.tableView addFooterWithTarget:self action:@selector(footerRefreshing)];
+    
+}
+
+- (void)headerRefreshing {
+
+    NSLog(@"进入程序就自动刷新！");
+    
+}
+
+- (void)footerRefreshing {
+
+    NSLog(@"上拉加载更多");
+    
 }
 
 @end

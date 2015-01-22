858c4fb1f3732d3065123063171aac89346324c7
iHannibal <fredisstillalmighty@gmail.com>
2015-01-22 23:30:04 +0800

    XMPP

---
+++ XMPP/XMPP/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XMPP
+//
+//  Created by Frankenstein Yang on 1/22/15.
+//  Copyright (c) 2015 Frankenstein Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
